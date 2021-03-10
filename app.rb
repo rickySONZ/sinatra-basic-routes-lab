156a1a93ee9024ebe923af4bb7ea06ca848798f2
Ryan Erricson <ryanerricson@gmail.com>
2021-03-10 15:16:49 -0500

    commit tree 37ceb0c7267d8bf67feeccd1ab1b03923b0a65c5

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/hometown' do
+        "My hometown is __"
+    end
+
+    get '/name' do
+        "My name is __"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is __"
+    end
+
 end
